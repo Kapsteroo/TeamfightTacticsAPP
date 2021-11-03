23e511b76383add97ab3265f3e7ab963f1828c62
Kapsteroo <kasperczykkacper@gmail.com>
2021-11-03 20:18:47 +0100

    firebase conection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'teamfighttacticsapp-90379',
+    appId: '1:850372640521:web:a1c3143c008c545aca5dd8',
+    storageBucket: 'teamfighttacticsapp-90379.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyDhkwcoN37BfHLpMnQzx18A6_c_Uq_trAE',
+    authDomain: 'teamfighttacticsapp-90379.firebaseapp.com',
+    messagingSenderId: '850372640521',
+    measurementId: 'G-W337Q0F4Y3',
+  },
   production: true
 };
