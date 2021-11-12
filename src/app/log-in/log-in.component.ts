a006ceefdf6ee69c91f6dc8c2e1c3ffd81726a33
Kapsteroo <kasperczykkacper@gmail.com>
2021-11-12 17:45:38 +0100

    Finished logging process, CSS works needed

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
+  styleUrls: ['./log-in.component.scss'],
 })
 export class LogInComponent implements OnInit {
+  loginForm: FormGroup;
+  firebaseErrorMessage: string;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private afAuth: AngularFireAuth
+  ) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
 
-  ngOnInit(): void {
+    this.firebaseErrorMessage = '';
   }
 
+  ngOnInit(): void {}
+
+  loginUser() {
+    if (this.loginForm.invalid) return;
+
+    this.authService
+      .loginUser(this.loginForm.value.email, this.loginForm.value.password)
+      .then((result) => {
+        if (result == null) {
+          // null is success, false means there was an error
+          console.log('logging in...');
+          this.router.navigate(['/home']); // when the user is logged in, navigate them to home
+        } else if (result.isValid == false) {
+          console.log('login error', result);
+          this.firebaseErrorMessage = result.message;
+        }
+      });
+  }
 }
