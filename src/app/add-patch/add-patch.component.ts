26da40d4859d526d0fcffa71dd61feb4fb17f8e3
Kapsteroo <kasperczykkacper@gmail.com>
2021-11-23 20:54:18 +0100

    User can add new patch via web application (not css included)

--- src/app/add-patch/add-patch.component.ts
+++ src/app/add-patch/add-patch.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PatchService } from '../services/patch.service';
 import { Patch } from '../models/patch';
 
@@ -8,15 +8,35 @@ import { Patch } from '../models/patch';
   styleUrls: ['./add-patch.component.scss'],
 })
 export class AddPatchComponent implements OnInit {
+  @ViewChild('desc') inputDesc: any;
   patch: Patch = {
     title: '',
     description: [],
     // id: ''
   };
+  newDescription: '';
 
   constructor(private patchService: PatchService) {}
 
   ngOnInit(): void {}
+
+  // onKeyUp(x: any) {
+  //   this.newDescription = x.target.value;
+  //   console.log(this.newDescription);
+  //   this.patch.description.push(this.newDescription);
+  // }
+
+  handleClear() {
+    this.newDescription = this.inputDesc.nativeElement.value;
+    console.log(this.newDescription);
+    this.patch.description.push(this.newDescription);
+    this.inputDesc.nativeElement.value = '';
+  }
+  // addDescriptionToArray(x: any) {
+  //   this.newDescription = x.target.value;
+  //   console.log(this.newDescription);
+  //   this.patch.description.push(this.newDescription);
+  // }
 
   onSubmit() {
     if (this.patch.title != '') {
